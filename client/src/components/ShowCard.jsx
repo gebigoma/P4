1131e09e098d017a5332347578125dbc175b46a9
Tina <tparkmedia@gmail.com>
2018-07-29 19:35:34 -0700

    componentized social media links from Show page

--- client/src/components/ShowCard.jsx
+++ client/src/components/ShowCard.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Container } from 'semantic-ui-react'
+import SocialMedia from './SocialMedia'
 import {
   FacebookShareButton,
   TwitterShareButton,
@@ -24,94 +26,19 @@ class ShowCard extends Component {
   }
 
   render() {
+    
   const { submission } = this.props;
-  const shareUrl = window.location.href;
-  const title = submission.title;
-  const hashtag = "#sugarfreegallery";
-  const body = submission.body
-  const media = submission.featuredImageUrl
 
     return (
-      <div>
+      <Container >
         <h1>{submission.title}</h1>
         <p>{submission.body}</p>
         <p> <a href={this.formatLink(submission.post_url)} target="_blank"> <img alt={submission.title} src={submission.featuredImageUrl} /> </a></p>
         <p>{submission.tags.join(', ')}</p>
         <p>{submission._by.name}</p>
       
-      <div className="display">
-        <FacebookShareButton 
-          quote={title}
-          url={shareUrl}
-          hashtag={hashtag} 
-          className="share-button">
-            <FacebookIcon 
-            size={32}
-            round/> 
-        </ FacebookShareButton>
-        </div>
-
-        <div className="display">
-        <TwitterShareButton
-          url={shareUrl}
-          title={title}
-          className="share-button">
-          <TwitterIcon
-            size={32}
-            round />
-        </TwitterShareButton>
-        </div>
-
-        <div className="display">
-        <PinterestShareButton
-          url={shareUrl}
-          media={media}
-          windowWidth={1000}
-          windowHeight={730}
-          className="share-button">
-          <PinterestIcon size={32} round />
-        </PinterestShareButton>
-        </div>
-
-        <div className="display">
-        <RedditShareButton
-          url={shareUrl}
-          title={title}
-          windowWidth={660}
-          windowHeight={460}
-          className="share-button">
-          <RedditIcon
-            size={32}
-            round />
-        </RedditShareButton>
-        </div>
-
-        <div className="display">
-        <TumblrShareButton
-          url={shareUrl}
-          title={title}
-          windowWidth={660}
-          windowHeight={460}
-          className="share-button">
-          <TumblrIcon
-            size={32}
-            round />
-        </TumblrShareButton>
-        </div>
-
-        <div className="display">
-        <EmailShareButton
-          url={shareUrl}
-          subject={title}
-          body={body}
-          className="share-button">
-          <EmailIcon
-            size={32}
-            round />
-        </EmailShareButton>
-        </div>
-
-      </div>
+      <SocialMedia submission={submission} />
+    </ Container>
     )
   } 
 }
--- client/src/components/SocialMedia.jsx
+++ client/src/components/SocialMedia.jsx
@@ -21,82 +21,87 @@ class SocialMedia extends Component {
   
   
   render() {
-    const { title, shareUrl, hashtag, body } = this.props
+    const { submission } = this.props
+    const shareUrl = window.location.href;
+    const title = submission.title;
+    const hashtag = "#sugarfreegallery";
+    const body = submission.body
+    const media = submission.featuredImageUrl  
+
     return (
       <div>
-        <div className="display">
-          <FacebookShareButton
-            quote={title}
-            url={shareUrl}
-            hashtag={hashtag}
-            className="share-button">
-            <FacebookIcon
-              size={32}
-              round />
-          </ FacebookShareButton>
+       <div className="display">
+        <FacebookShareButton 
+          quote={title}
+          url={shareUrl}
+          hashtag={hashtag} 
+          className="share-button">
+            <FacebookIcon 
+            size={32}
+            round/> 
+        </ FacebookShareButton>
         </div>
 
         <div className="display">
-          <TwitterShareButton
-            url={shareUrl}
-            title={title}
-            className="share-button">
-            <TwitterIcon
-              size={32}
-              round />
-          </TwitterShareButton>
+        <TwitterShareButton
+          url={shareUrl}
+          title={title}
+          className="share-button">
+          <TwitterIcon
+            size={32}
+            round />
+        </TwitterShareButton>
         </div>
 
         <div className="display">
-          <PinterestShareButton
-            url={String(window.location)}
-            media={`${String(window.location)}/${submission.featuredImageUrl}`}
-            windowWidth={1000}
-            windowHeight={730}
-            className="share-button">
-            <PinterestIcon size={32} round />
-          </PinterestShareButton>
+        <PinterestShareButton
+          url={shareUrl}
+          media={media}
+          windowWidth={1000}
+          windowHeight={730}
+          className="share-button">
+          <PinterestIcon size={32} round />
+        </PinterestShareButton>
         </div>
 
         <div className="display">
-          <RedditShareButton
-            url={shareUrl}
-            title={title}
-            windowWidth={660}
-            windowHeight={460}
-            className="share-button">
-            <RedditIcon
-              size={32}
-              round />
-          </RedditShareButton>
+        <RedditShareButton
+          url={shareUrl}
+          title={title}
+          windowWidth={660}
+          windowHeight={460}
+          className="share-button">
+          <RedditIcon
+            size={32}
+            round />
+        </RedditShareButton>
         </div>
 
         <div className="display">
-          <TumblrShareButton
-            url={shareUrl}
-            title={title}
-            windowWidth={660}
-            windowHeight={460}
-            className="share-button">
-            <TumblrIcon
-              size={32}
-              round />
-          </TumblrShareButton>
+        <TumblrShareButton
+          url={shareUrl}
+          title={title}
+          windowWidth={660}
+          windowHeight={460}
+          className="share-button">
+          <TumblrIcon
+            size={32}
+            round />
+        </TumblrShareButton>
         </div>
 
         <div className="display">
-          <EmailShareButton
-            url={shareUrl}
-            subject={title}
-            body={body}
-            className="share-button">
-            <EmailIcon
-              size={32}
-              round />
-          </EmailShareButton>
+        <EmailShareButton
+          url={shareUrl}
+          subject={title}
+          body={body}
+          className="share-button">
+          <EmailIcon
+            size={32}
+            round />
+        </EmailShareButton>
         </div>
       </div>
-
     )
   }
 }
